974fba2832ec1fd431ad30aed4a3c90128593170
Mike Wilmer <mwilmer@gmail.com>
2014-08-25 16:47:05 +0200

    Tried to add a texstudio cask. Ready for testing.

---
+++ Casks/texstudio.rb
@@ -0,0 +1,10 @@
+class Texstudio < Cask
+  version '2.8.2'
+  sha256 '70a95e7602a790b71744ce84a649dfe5f7b597caa42007e3c34c3e631ffadb22'
+
+  url 'http://downloads.sourceforge.net/project/texstudio/texstudio/TeXstudio%202.8.2/texstudio_2.8.2_osx_qt5.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Ftexstudio%2Ffiles%2Ftexstudio%2FTeXstudio%25202.8.2%2F&ts=1408977845&use_mirror=switch'
+
+  homepage 'http://texstudio.sourceforge.net/'
+
+  link 'texstudio.app'
+end
