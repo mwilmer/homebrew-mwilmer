a8267bdaca4843bde76ee24c5351000daab2fbcd
Mike Wilmer <mwilmer@gmail.com>
2014-08-25 20:03:05 +0200

    Changed to latest.

--- Casks/texstudio.rb
+++ Casks/texstudio.rb
@@ -1,8 +1,8 @@
 class Texstudio < Cask
-  version '2.8.2'
-  sha256 '0ecd43af8a21e096f9d5382610297532a49275fc9fc672b9c7882f3776e180be'
+  version 'latest'
+  sha256 :nocheck
 
-  url 'http://downloads.sourceforge.net/project/texstudio/texstudio/TeXstudio%202.8.2/texstudio_2.8.2_osx_qt5.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Ftexstudio%2Ffiles%2Ftexstudio%2FTeXstudio%25202.8.2%2F&ts=1408977845&use_mirror=switch'
+  url 'http://sourceforge.net/projects/texstudio/files/latest/download'
 
   homepage 'http://texstudio.sourceforge.net/'
 
